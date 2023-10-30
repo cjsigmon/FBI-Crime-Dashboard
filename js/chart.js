0b82ef89750278617c852ab8ae2072b19e612da1
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-10-30 11:47:30 -0400

    first commit

---
+++ js/chart.js
@@ -0,0 +1,59 @@
+var ctx = document.getElementById('myChart');
+const yearsIndex = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
+var myChart = null;
+
+
+
+$(document).ready(function () {
+    console.log("society data reached");
+
+});
+
+function parseData(json, i) {
+    console.log("new load")
+    console.log(json); 
+
+    const valuesArray = Object.values(json.data[i]);
+
+    console.log("vals is");
+    console.log(valuesArray);
+
+    if (!myChart) {
+      myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: json.keys,
+          datasets: [{
+            label: json.title,
+            data: Object.values(json.data[i]),
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              suggestedMax: 3500000 // Set the maximum value for the y-axis
+            }
+          }
+        }
+      });
+    } else {
+      console.log("else reached")
+      myChart.data.datasets[0].data = Object.values(json.data[i]);
+      myChart.update();
+    }
+
+    } 
+
+    
+
+    // for (let i = 0; i < 25; i++) {
+    //     let newReport = "Crime committed: " + json.data[i][CRIME] + " by " +json.data[i][AGE]
+    //     + " years old " + json.data[i][RACE] + " " + json.data[i][SEX];
+    //     var li = document.createElement("li");
+    //     li.appendChild(document.createTextNode(newReport));
+    //     CRIMELIST.appendChild(li);
+    // }
+
+
---
+++ js/main.js
@@ -0,0 +1,44 @@
+let jsonRef;
+var CRIMELIST = document.getElementById("crimeList");
+const CRIME = 11;
+const AGE = 12;
+const SEX = 13;
+const RACE = 14;
+const API_KEY = "edmV3kgmSrqftZzFIox9HEqSxJbhxf9VYCbKIb1Z";
+const yearRange = document.getElementById("yearRange");
+var time = 1;
+var DATA = null;
+
+
+
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "https://api.usa.gov/crime/fbi/cde/arrest/national/society",
+        data: {
+            from: 2012,
+            to: 2023,
+            API_KEY: API_KEY
+        },
+        dataType: "json",
+        success: function (json) {
+            console.log("success on ajax");
+            DATA = json;
+
+            parseData(DATA, 0);
+
+        }
+    }); 
+
+});
+
+function onValueChanged() {
+    const value = yearRange.value;
+    console.log("Value is "+ value);
+    parseData(DATA, value);
+    // Add additional code here to handle the value change as needed.
+  }
+
+yearRange.addEventListener('input', onValueChanged);
+
+
